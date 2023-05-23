ec687e5cf2018cb94eacd98ad3c5294a3b48576e
Shuai Lu <72731881+seth-lu@users.noreply.github.com>
2023-05-23 09:25:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -D CMAKE_PREFIX_PATH=~/libtorch ../benchmarks # Modify to the absolute path of ‘libtorch’
+cmake -D CMAKE_PREFIX_PATH=/home/pytorch/libtorch/ ../benchmarks # Modify to the absolute path of ‘libtorch’
 make
